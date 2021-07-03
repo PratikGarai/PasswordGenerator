408b1cc358570793f9b7799a80cf1118b4847a89
PratikGarai <pratikgarai0208@gmail.com>
2021-07-03 11:10:28 +0530

    Functionality done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,47 @@ export class AppComponent {
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
+  length = 0;
   password = '';
 
   onButtonClick() {
-    this.password = "RandomString";
-    console.log({
-      letters : this.includeLetters, 
-      numbers : this.includeNumbers, 
-      symbols : this.includeSymbols
-    });
+    this.password = this.generatePassword();
+    // console.log({
+    //   length : this.length,
+    //   letters : this.includeLetters, 
+    //   numbers : this.includeNumbers, 
+    //   symbols : this.includeSymbols
+    // });
+  }
+
+  generatePassword() {
+    const numbers = "124567890";
+    const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const symbols = "!@#$%^&*-+=?|";
+
+    let validchars = '';
+    if(this.includeLetters)
+      validchars += letters;
+    if(this.includeNumbers)
+      validchars += numbers;
+    if(this.includeSymbols)
+      validchars += symbols;
+
+    let pwd = "";
+    let l = validchars.length;
+    for(let i=0; i<this.length; i++) {
+      const index = Math.floor(Math.random() * l);
+      pwd += validchars[index];
+    }
+    console.log(pwd, validchars);
+    return pwd;
+  }
+
+  onChangeLength(event : any) {
+    const parsedValue = parseInt(event.target.value);
+
+    if(!isNaN(parsedValue))
+      this.length = parsedValue;
   }
 
   onChangeUseLetters() {
