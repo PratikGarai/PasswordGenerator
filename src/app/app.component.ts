1b5ccf1bb773f3e0570d032f08756d13cdc6e58e
PratikGarai <pratikgarai0208@gmail.com>
2021-07-03 10:55:48 +0530

    Simpler handlers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'PasswordGenerator';
+
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onButtonClick() {
+    this.password = "RandomString";
+    console.log({
+      letters : this.includeLetters, 
+      numbers : this.includeNumbers, 
+      symbols : this.includeSymbols
+    });
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
 }
